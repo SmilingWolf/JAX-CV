1540f84c3d614e05ea643ee16f64f9e9256be636
SmilingWolf <lupo996@gmail.com>
2023-10-21 08:10:46 +0200

    ViT: initial import of model and training code

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,6 +1,10 @@
 from .SwinV2 import swinv2_base_window8_256, swinv2_tiny_window8_256
+from .ViT import vit_base, vit_large, vit_small
 
 model_registry = {
     "swinv2_tiny": swinv2_tiny_window8_256,
     "swinv2_base": swinv2_base_window8_256,
+    "vit_small": vit_small,
+    "vit_base": vit_base,
+    "vit_large": vit_large,
 }
